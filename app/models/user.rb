a0464eda903ddd93d74fa23733d270afbe482a36
himanoa <matsunoappy@gmail.com>
2017-04-16 17:02:25 +0900

    activate secure password in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true, uniqueness: true
