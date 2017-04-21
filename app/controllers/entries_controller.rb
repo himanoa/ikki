27526c17e7d983af8888df5e52d3c048994ef6b9
himanoa <matsunoappy@gmail.com>
2017-04-22 05:50:55 +0900

    add entry create test

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class EntriesController < ApplicationController
+  def new; end
+
+  def create; end
+
+  def edit; end
 end
--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -3,4 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe EntriesController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
+  describe 'POST# create' do
+    context 'logined' do
+      before do
+        session = { user_id: 2 }
+        add_session(session)
+      end
+      it 'return success when created entries' do
+        post :create, FactoryGirl.build(:entries).attribute
+        expect(response).to have_http_status(201)
+        expect(Entries.count).to eq(1)
+        expect(Entries.first.user.id).to eq(2)
+      end
+    end
+    context 'loggout' do
+      before do
+        session = { user_id: nil }
+        add_session(session)
+      end
+      it 'return fail when created entries' do
+        expect(response).to have_http_status(403)
+        expect(Entries.count).to eq(0)
+      end
+    end
+  end
 end
