ccd385cfbe7b82116fa55f6dee5c1c72369cff0c
himanoa <matsunoappy@gmail.com>
2017-09-08 02:33:43 +0900

    add test case

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -30,4 +30,32 @@ RSpec.describe SessionsController, type: :controller do
       expect(session['user_id']).to eq(nil)
     end
   end
+
+  describe 'POST #create' do
+    let(:user) { User.create(name: 'himanoa', email: 'matsunoappy@gmail.com', password: 'a', password_confirmation: 'a') }
+
+    subject { post :create, params: params }
+
+    after do
+      session['user_id'] = nil
+    end
+
+    context 'valid' do
+      let(:params) { { session: { email: user.email, password: user.password } } }
+      it 'redirect to root_path' do
+        expect(subject).to have_http_status(302)
+        post :create, params: params
+        expect(session['user_id']).to eq(user.id)
+      end
+    end
+
+    context 'invalid' do
+      let(:params) { { session: { email: "#{user.email}invalid", password: "#{user.password}invalid" } } }
+      it 'redirect to root_path' do
+        expect(subject).to have_http_status(200)
+        expect(session['user_id']).to eq(nil)
+        expect(subject).to render_template('sessions/new')
+      end
+    end
+  end
 end
