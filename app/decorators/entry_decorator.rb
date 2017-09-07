69171fedbb6fbdc1cebf1011aabd758477877e10
himanoa <matsunoappy@gmail.com>
2017-09-08 06:43:21 +0900

    implement index page

--- app/decorators/entry_decorator.rb
+++ app/decorators/entry_decorator.rb
@@ -6,10 +6,6 @@ class EntryDecorator < Draper::Decorator
   FORMAT = '%Y/%m/%d %H:%M:%S'
   DESC_LENGTH = 49
 
-  def cache_prefix
-    "#{model.id}-#{model.updated_at}"
-  end
-
   def format_updated_at
     model.created_at.strftime(FORMAT)
   end
@@ -19,16 +15,14 @@ class EntryDecorator < Draper::Decorator
   end
 
   def cached_body
-    Rails.cache.fetch('decorators/entry/body/#{cache_prefix}') do
+    Rails.cache.fetch('decorators/entry/body/#{model.cache_key}') do
       model.to_html
     end
   end
 
   def description
-    Rails.cache.fetch('decorators/entry/description/#{cache_prefix}') do
-      short_body = model.to_html.css('p').reduce([]) { |_, val| val.children.to_s }.slice(0..DESC_LENGTH)
-      next "#{short_body}‥" if short_body.length == DESC_LENGTH
-      short_body
-    end
+    @short_body ||= model.to_html.css('p').reduce([]) { |_, val| val.children.to_s }.slice(0..DESC_LENGTH)
+    return "#{@short_body}‥" if @short_body.length == DESC_LENGTH
+    @short_body
   end
 end
