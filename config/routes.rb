878e8eed2d2da5fbe791875b8dfceb48f2edc502
himanoa <matsunoappy@gmail.com>
2017-09-08 22:31:50 +0900

    add sidekiq dashboard routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   root 'pages#index'
   resources :users, :entries, :pages
+  scope '/manage' do
+    mount Sidekiq::Web, at: "/sidekiq"
+  end
 end
