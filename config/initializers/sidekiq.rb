4bb59134b85567008804f35cfab79540197016df
himanoa <matsunoappy@gmail.com>
2017-09-10 12:27:39 +0900

    add sitemap schedule

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+require 'yaml'
+Sidekiq.configure_server do |config|
+  schedule_file = "config/schedule.yml"
+
+  if File.exists?(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
+end
