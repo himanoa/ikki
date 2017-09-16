e5fcea99a34c78b00c0fa0071264285e56f39e5f
himanoa <matsunoappy@gmail.com>
2017-09-16 22:30:15 +0900

    sitemap

--- app/jobs/sitemap_job.rb
+++ app/jobs/sitemap_job.rb
@@ -2,7 +2,7 @@ class SitemapJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Rails.application.load_tasksend
+    Rails.application.load_tasks
     Rake::Task['sitemap:refresh'].invoke
   end
 end
