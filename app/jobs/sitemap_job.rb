25a7bad5a99ec25c7012f17931ee3b9b61011a7a
himanoa <matsunoappy@gmail.com>
2017-09-16 22:36:45 +0900

    rubocop -a

--- app/jobs/sitemap_job.rb
+++ app/jobs/sitemap_job.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class SitemapJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(*_args)
     Rails.application.load_tasks
     Rake::Task['sitemap:refresh'].invoke
   end
